f389bebec3ba7e7da13c2dd3b145fa20e0d70b02
Tzruya <109286665+Tzruya@users.noreply.github.com>
2023-05-04 15:07:29 +0300

    Create shiur2.py

---
+++ shiur2.py
@@ -0,0 +1,10 @@
+# 1. כתוב תוכנית המגדירה שני מספרים , אחד-מסוג שלם והשני מסוג עשרוני המדפיסה את מכפלתם
+
+x=5
+y=5.5
+print(x*y)   #  output 27.5
+
+# 2. כתוב תוכנית הכוללת מחרוזת אחת עם התוכן "Proud to live in israel" ומחרוזת שניה עם התוכן "and happy to study Computer Science"  אשר מדפיסה למסך את השרשור של 2 המחרוזות.
+str1="Proud to live in israel"
+str2="and happy to study Computer Science"
+print(str1,str2)
